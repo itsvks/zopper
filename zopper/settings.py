9e093abc70e30d789537b7dd5b3bff61e8b25590
root <root@localhost.localdomain>
2015-03-24 15:03:56 +0530

    Updated settings.py file

--- zopper/settings.py
+++ zopper/settings.py
@@ -58,8 +58,7 @@ WSGI_APPLICATION = 'zopper.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://foo:bar@localhost:5432/db')}
-
+DATABASES['default'] =  dj_database_url.config()
 
 #DATABASES = {
 #    'default': {
@@ -86,6 +85,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
