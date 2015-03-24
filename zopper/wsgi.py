92f7046bedce902730f4a29540dcb911048ba911
root <root@localhost.localdomain>
2015-03-24 15:33:25 +0530

    Updated wsgi.py file

--- zopper/wsgi.py
+++ zopper/wsgi.py
@@ -13,9 +13,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zopper.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-"""
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
-"""
