3c3991e1a250b8ac0b87e2c056b29d1e85793cab
vikas <vikas@localhost.localdomain>
2015-03-25 11:11:05 +0530

    Updated admin file

--- zopper_app/admin.py
+++ zopper_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from zopper_app.models import Device
 # Register your models here.
+
+class DeviceAdmin(admin.ModelAdmin):
+	model = Device
+	list_display = ('id', 'device_name', 'range')
+	ordering = ('id',)
+
+admin.site.register(Device, DeviceAdmin)
